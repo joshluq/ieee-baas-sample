9e7901daae24879107aaeed78e6b711c6c835cfe
JorgeLuisDAC <jorge.luis.del.aguila.cardenas@everis.com>
2018-07-12 18:51:01 -0500

    Estructura

--- app/src/main/java/com/everis/example/baasandroidsample/activity/MainActivity.java
+++ app/src/main/java/com/everis/example/baasandroidsample/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.everis.example.baasandroidsample;
+package com.everis.example.baasandroidsample.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.everis.example.baasandroidsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
