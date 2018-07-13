7fa886779e6eb78bb2cd32d184cc96850d000206
jluquere@everis.com <joshluq123>
2018-07-12 23:18:16 -0500

    view

---
+++ app/src/main/java/com/everis/example/baasandroidsample/fragment/SignUpFragment.java
@@ -0,0 +1,29 @@
+package com.everis.example.baasandroidsample.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.everis.example.baasandroidsample.R;
+
+public class SignUpFragment extends Fragment {
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+    public static SignUpFragment newInstance() {
+        return new SignUpFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login,container,false);
+    }
+
+
+}
